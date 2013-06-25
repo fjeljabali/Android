eaccd64dd51b142c4a7db3b2557ec10cadbd63d3
F. Julian Eljabali <julian.jabali@gmail.com>
2013-06-25 11:39:37 -0700

    Improved layout

--- RandomCircles/src/com/randomcircles/CircleGenerator.java
+++ RandomCircles/src/com/randomcircles/CircleGenerator.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
-import android.graphics.Color;
+import android.util.AttributeSet;
+
+//import android.graphics.Color;
 
 public class CircleGenerator extends Activity 
 {
@@ -13,22 +15,25 @@ public class CircleGenerator extends Activity
 	protected void onCreate(Bundle b) 
 	{
 		super.onCreate(b);
-		drawcircle = new DrawCircle(this);
+		drawcircle = new DrawCircle(this, null);
 		setContentView(R.layout.activity_circle_generator);
 	}
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) 
-//	{
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.circle_generator, menu);
-//		return true;
-//	}
-//	
-//	public void pushmebutton(View v)
-//	{
-//		
-//	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.circle_generator, menu);
+		return true;
+	}
+	
+	//public void pushmebutton(View v)
+	//{
+		//
+	    //drawcircle = new DrawCircle(this);
+		//setContentView(R.layout.activity_circle_generator);
+
+	//}
 	
 
 }
--- RandomCircles/src/com/randomcircles/DrawCircle.java
+++ RandomCircles/src/com/randomcircles/DrawCircle.java
@@ -4,15 +4,16 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class DrawCircle extends View 
 {
     Paint paint = new Paint();
 
-    public DrawCircle(Context context) 
+    public DrawCircle(Context context, AttributeSet attrs) 
     {
-       super(context);
+       super(context, attrs);
        paint.setColor(Color.YELLOW);
     }
 
