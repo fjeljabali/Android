834e1e173e555238654a37a27795ab0e7622995e
F. Julian Eljabali <julian.jabali@gmail.com>
2013-07-15 12:14:22 -0700

    Photo album complete and ready to use

--- PhotoAlbum/gen/com/photoalbum/R.java
+++ PhotoAlbum/gen/com/photoalbum/R.java
@@ -31,27 +31,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int pic1=0x7f020001;
-        public static final int pic2=0x7f020002;
-        public static final int pic3=0x7f020003;
-        public static final int pic4=0x7f020004;
-        public static final int pic5=0x7f020005;
-        public static final int pic6=0x7f020006;
-        public static final int pic7=0x7f020007;
-        public static final int welcome=0x7f020008;
+        public static final int nexticon=0x7f020001;
+        public static final int pic1=0x7f020002;
+        public static final int pic2=0x7f020003;
+        public static final int pic3=0x7f020004;
+        public static final int pic4=0x7f020005;
+        public static final int pic5=0x7f020006;
+        public static final int pic6=0x7f020007;
+        public static final int pic7=0x7f020008;
+        public static final int previcon=0x7f020009;
+        public static final int welcome=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000a;
-        public static final int nextButton=0x7f090009;
-        public static final int pic1=0x7f090001;
-        public static final int pic2=0x7f090002;
-        public static final int pic3=0x7f090003;
-        public static final int pic4=0x7f090004;
-        public static final int pic5=0x7f090005;
-        public static final int pic6=0x7f090006;
-        public static final int pic7=0x7f090007;
-        public static final int prevButton=0x7f090008;
-        public static final int welcome=0x7f090000;
+        public static final int action_settings=0x7f090003;
+        public static final int images=0x7f090000;
+        public static final int nextbutton=0x7f090002;
+        public static final int prevbutton=0x7f090001;
     }
     public static final class layout {
         public static final int activity_photo_album=0x7f030000;
--- PhotoAlbum/src/com/photoalbum/PhotoAlbum.java
+++ PhotoAlbum/src/com/photoalbum/PhotoAlbum.java
@@ -1,71 +1,78 @@
 package com.photoalbum;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class PhotoAlbum extends Activity 
 {
-   Integer pics[] = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3+
-		             R.drawable.pic4, R.drawable.pic5, R.drawable.pic6+
-		             R.drawable.pic7};
+   ImageView iv;
+   Integer counter=0;
+   
+   Integer pics[] = {
+		               R.drawable.pic1, 
+		               R.drawable.pic2, 
+		               R.drawable.pic3,
+		               R.drawable.pic4, 
+		               R.drawable.pic5,
+		               R.drawable.pic6,
+		               R.drawable.pic7,
+		            };
 
-/*********************************onCreate()*********************************/
+//*********************************onCreate()*********************************//
    @Override
    protected void onCreate(Bundle b) 
    {
       super.onCreate(b);
 	  setContentView(R.layout.activity_photo_album);
+	  iv = (ImageView)findViewById(R.id.images);
+	  
+	  ImageButton nextbutton = (ImageButton)findViewById(R.id.nextbutton);
+	  ImageButton prevbutton = (ImageButton)findViewById(R.id.prevbutton);
+    
+//*************************goClickListener()*************************//     
+
+      nextbutton.setOnClickListener(new OnClickListener()
+      {
+         @Override
+    	 public void onClick(View v)
+    	 {        	 
+            if (counter < pics.length)
+    	    {
+    	       counter++;
+    	    }
+    	    iv.setImageResource(pics[counter]);
+    	 }
+      });
+      
+//*************************goClickListener()*************************//     
+	  prevbutton.setOnClickListener(new OnClickListener()
+      {
+         @Override
+    	 public void onClick(View v)
+    	 {
+        	 if(counter>0)
+   	         {
+   	            counter--;
+   	         }
+   	         iv.setImageResource(pics[counter]);
+    	 }
+      });
    }
    
-/****************************onCreateOptionMenu()****************************/
+
+//****************************onCreateOptionMenu()****************************/
    @Override
    public boolean onCreateOptionsMenu(Menu menu) 
    {
       // Inflate the menu; this adds items to the action bar if it is present.
       getMenuInflater().inflate(R.menu.photo_album, menu);
       return true;
-   }
-/******************************buttonHandler()******************************/
-   public void buttonHandler(View v)
-   {
-      switch (v.getId())
-      {
-   	     case R.id.prevButton: countDown() ; break;
-   	     case R.id.nextButton: countUp() ; break;
-      }
-   }
-/******************************countUp()******************************/
-   public void countUp()
-   {
-      for(Integer p:pics)
-      {
-         ImageView iv = (ImageView)findViewById(p);
-    	 iv.setImageResource(p);
-         sleep(500);
-         p++;
-      }
-   }
-/******************************countDown()******************************/
-   public void countDown()
-   {
-      for(Integer p:pics)
-	  {
-         ImageView iv = (ImageView)findViewById(p);
-    	 iv.setImageResource(p);
-	     sleep(500);
-	     p--;
-	  }
-   }
-/*********************************sleep()*********************************/
-   public static void sleep(long msec)
-   {
-      try
-      {
-         Thread.sleep(msec);
-      }catch(InterruptedException e){}
-   }
+   }   
       
 }
