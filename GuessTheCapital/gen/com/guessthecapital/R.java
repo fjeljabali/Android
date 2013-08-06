3806db77c4395f5e890fa2dd21d4cc9376f722d1
F. Julian Eljabali <julian.jabali@gmail.com>
2013-08-06 15:20:12 -0700

    Database and intents updated

--- GuessTheCapital/gen/com/guessthecapital/R.java
+++ GuessTheCapital/gen/com/guessthecapital/R.java
@@ -11,13 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040005;
-        public static final int blue=0x7f040002;
-        public static final int green=0x7f040001;
-        public static final int purple=0x7f040004;
-        public static final int red=0x7f040000;
-        public static final int seablue=0x7f040003;
-        public static final int white=0x7f040006;
+        public static final int black=0x7f050005;
+        public static final int blue=0x7f050002;
+        public static final int green=0x7f050001;
+        public static final int purple=0x7f050004;
+        public static final int red=0x7f050000;
+        public static final int seablue=0x7f050003;
+        public static final int white=0x7f050006;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -26,8 +26,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -36,25 +36,36 @@ public final class R {
         public static final int start=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int entername=0x7f090002;
-        public static final int logo=0x7f090000;
-        public static final int name=0x7f090001;
-        public static final int scoreboard=0x7f090003;
-        public static final int start=0x7f090004;
+        public static final int action_settings=0x7f0a0009;
+        public static final int answer=0x7f0a0002;
+        public static final int entername=0x7f0a0006;
+        public static final int logo=0x7f0a0004;
+        public static final int name=0x7f0a0005;
+        public static final int next=0x7f0a0003;
+        public static final int question=0x7f0a0001;
+        public static final int scoreboard=0x7f0a0007;
+        public static final int start=0x7f0a0008;
+        public static final int title=0x7f0a0000;
     }
     public static final class layout {
-        public static final int activity_start=0x7f030000;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_start=0x7f030001;
     }
     public static final class menu {
-        public static final int start=0x7f080000;
+        public static final int game=0x7f090000;
+        public static final int start=0x7f090001;
+    }
+    public static final class raw {
+        public static final int us_states=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int desc=0x7f060004;
-        public static final int entername=0x7f060003;
-        public static final int logo=0x7f060002;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int desc=0x7f070004;
+        public static final int entername=0x7f070003;
+        public static final int hello_world=0x7f070006;
+        public static final int logo=0x7f070002;
+        public static final int title_activity_game=0x7f070005;
     }
     public static final class style {
         /** 
@@ -78,10 +89,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- GuessTheCapital/src/com/guessthecapital/StartActivity.java
+++ GuessTheCapital/src/com/guessthecapital/StartActivity.java
@@ -1,8 +1,22 @@
+//**************************************************
+//Name: Julian Eljabali                           **
+//Assignment: #5                                  **
+//July 17th 2013                                  **
+//**************************************************
+
 package com.guessthecapital;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,6 +28,10 @@ public class StartActivity extends Activity
    EditText name;
    ImageButton start;
    ImageButton scoreboard;
+   ArrayList<String> states = new ArrayList<String>();
+   ArrayList<String> capitals = new ArrayList<String>();
+   String[] split;
+   Intent NameIntent;
       
 //************************onCreate()************************//
    @Override
@@ -22,9 +40,45 @@ public class StartActivity extends Activity
       super.onCreate(savedInstanceState);
 	  setContentView(R.layout.activity_start);
 	  
-      final EditText name = (EditText)findViewById(R.id.name);
 	  ImageButton start = (ImageButton)findViewById(R.id.start);
       ImageButton scoreboard = (ImageButton)findViewById(R.id.scoreboard);
+      
+	  SQLiteDatabase db = openOrCreateDatabase("statesandcaps.db",SQLiteDatabase.CREATE_IF_NECESSARY,null);
+		
+      String sqlcmd = "create table IF NOT EXISTS statesandcaps(_id integer " +
+                    "primary Key autoincrement, state text " +
+                    "not null, capital text);";
+      db.execSQL(sqlcmd);
+      
+      Cursor c = db.query("statesandcaps",null, null, 
+              null, null, null, null, null);
+      Resources resources = getApplicationContext().getResources();
+      InputStream is = resources.openRawResource(R.raw.us_states);
+      
+      Scanner sc = new Scanner(is);
+      ContentValues cv = new ContentValues();
+      c.moveToFirst();
+      
+      if(c.getCount()==0)
+      {
+          while(sc.hasNext())
+          {
+              String line = sc.nextLine();
+              split = line.split("-");
+              //Arrays of Strings
+              states.add(split[0]);
+              capitals.add(split[1]);
+              //Filling database
+              cv.put("state", split[0]);
+              cv.put("capital", split[1]);
+              db.insert("statesandcaps", null, cv);
+          }            
+      }
+      
+      NameIntent = new Intent(getApplicationContext(),
+              GameActivity.class);
+      name = (EditText)findViewById(R.id.name);
+		
     
 //***********************startClickListener()***********************//     
       start.setOnClickListener(new OnClickListener()
@@ -32,9 +86,7 @@ public class StartActivity extends Activity
          @Override
     	 public void onClick(View v)
     	 {
-            Intent i = new Intent (StartActivity.this, GameActivity.class);
-            i.putExtra("UserName", name.getText().toString());
-            startActivity(i);
+
             
     	 }
       });
