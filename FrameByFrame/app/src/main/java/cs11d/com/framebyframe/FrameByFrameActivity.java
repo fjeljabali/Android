dff23b94afc5bc321c9e5a409ebf6af230b93773
fjeljabali <fj.eljabali@gmail.com>
2015-03-04 14:42:40 -0800

    Seekbar functional

--- FrameByFrame/app/src/main/java/cs11d/com/framebyframe/FrameByFrameActivity.java
+++ FrameByFrame/app/src/main/java/cs11d/com/framebyframe/FrameByFrameActivity.java
@@ -1,3 +1,14 @@
+/* Julian Eljabali */
+/* Advanced Android */
+/* Assigment # 4 */
+/* March 4th 2015 */
+
+/***************************************
+ This app plays an animation of a boy
+ doing a back flip where the frames-per-second
+ is adjustable to go fast/slow
+ ***************************************/
+
 package cs11d.com.framebyframe;
 
 import android.support.v7.app.ActionBarActivity;
@@ -17,7 +28,7 @@ public class FrameByFrameActivity extends ActionBarActivity
 
     ImageView image_view;
     AnimationDrawable animation_drawable;
-    int PROGRESS_SPEED = 100;
+    int progress_speed;
     final int MAX_SPEED = 500;
 
     @Override
@@ -39,9 +50,9 @@ public class FrameByFrameActivity extends ActionBarActivity
     public void seek_Bar()
     {
         final SeekBar seek_bar = (SeekBar)findViewById(R.id.seek_bar);
-        final TextView maximum_text = (TextView)findViewById(R.id.maximum_text);
+        final TextView speed_text = (TextView)findViewById(R.id.speed_text);
 
-        maximum_text.setText("Covered: " + seek_bar.getProgress() + "/" + seek_bar.getMax());
+        speed_text.setText("Speed: " + seek_bar.getProgress() + "/" + seek_bar.getMax());
 
         seek_bar.setOnSeekBarChangeListener (new SeekBar.OnSeekBarChangeListener()
         {
@@ -49,8 +60,9 @@ public class FrameByFrameActivity extends ActionBarActivity
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
             {
-                PROGRESS_SPEED = progress;
-                maximum_text.setText("Covered: " + progress + "/" + seek_bar.getMax());
+                progress_speed = progress;
+                speed_text.setText("Speed: " + progress + "/" + seek_bar.getMax());
+                create_Animation();
             }
 
             @Override
@@ -61,7 +73,7 @@ public class FrameByFrameActivity extends ActionBarActivity
             @Override
             public void onStopTrackingTouch(SeekBar seekBar)
             {
-                maximum_text.setText("Covered: " + PROGRESS_SPEED + "/" + seek_bar.getMax());
+                speed_text.setText("Speed: " + progress_speed + "/" + seek_bar.getMax());
             }
         });
     }
@@ -96,35 +108,35 @@ public class FrameByFrameActivity extends ActionBarActivity
     {
         animation_drawable = new AnimationDrawable();
 
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file1), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file4), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file8), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file12), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file16), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file19), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file22), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file25), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file28), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file31), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file34), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file37), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file40), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file43), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file46), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file49), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file52), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file55), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file58), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file62), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file65), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file68), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file71), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file74), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file77), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file80), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file83), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file86), PROGRESS_SPEED);
-        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file89), PROGRESS_SPEED);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file1), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file4), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file8), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file12), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file16), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file19), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file22), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file25), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file28), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file31), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file34), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file37), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file40), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file43), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file46), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file49), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file52), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file55), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file58), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file62), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file65), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file68), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file71), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file74), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file77), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file80), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file83), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file86), progress_speed);
+        animation_drawable.addFrame(getResources().getDrawable(R.drawable.file89), progress_speed);
 
         animation_drawable.setOneShot(false);
         image_view.setBackground(animation_drawable);
